2ab2c497f7ffd07c698cdffa7ae88c816c15a141
lil-hummonkey <112558499+lil-hummonkey@users.noreply.github.com>
2023-05-26 13:32:31 +0200

    final

--- Jumpboy/Bullet.cs
+++ Jumpboy/Bullet.cs
@@ -1,6 +1,10 @@
 using System;
 
 
+
+//use of bullet class allows easier sorting of code
+
+//bullet variables are declared
 public class Bullet
 {
     public Vector2 pos;
@@ -10,7 +14,8 @@ public class Bullet
 
     public Rectangle circlelol;
 
-
+    //specific instance in bullet class of position/speed is the position/speed of all bullets.
+    //Bullets (circlelol) position is a variable of pos vector 
     public Bullet(Vector2 pos, Vector2 speed)
     {
         this.pos = pos;
@@ -18,6 +23,8 @@ public class Bullet
 
         circlelol = new Rectangle((int)pos.X - 2, (int)pos.Y - 2, 4, 4);
     }
+
+    //updates position according to pos vector
 
     public void Update()
     {
